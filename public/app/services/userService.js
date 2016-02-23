6fdf479cb366ae5be03d68d5dddeb8ce9ddccedc
Gary Pang <gary.pang@gmail.com>
2016-02-23 01:12:58 -0500

    Create AngularJS service
    Make new service. Inject it into main Angular module.

---
+++ public/app/services/userService.js
@@ -0,0 +1,15 @@
+angular.module('stuffService', [])
+
+	   .factory('Stuff', function($http){
+
+	   	// create the object
+	   	var myFactory = {};
+
+	   	// a function to get all the stuff
+	   	myFactory.all = function(){
+	   		return $http.get('/api/stuff');
+	   	};
+
+	   	// return promise object
+	   	return myFactory;
+   });
