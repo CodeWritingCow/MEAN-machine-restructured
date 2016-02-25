7b58b632019e34742cfb114ce813fcda4a3c8752
Gary Pang <gary.pang@gmail.com>
2016-02-24 23:05:49 -0500

    Create login route
    Add login route to app.routes.js. Also make login.html.

--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -7,7 +7,15 @@ angular.module('app.routes', ['ngRoute'])
 		// home page route
 		.when('/', {
 			templateUrl: 'app/views/pages/home.html'
+		})
+
+		// login page route
+		.when('/login', {
+			templateUrl: 'app/views/pages/login.html',
+			controller: 'mainController',
+			controllerAs: 'login'
 		});
+
 
 		// get rid of the hash in the URL
 		$locationProvider.html5Mode(true);
